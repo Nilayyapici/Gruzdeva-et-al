d8fb2aef02a02fd487ea3ca051630b212390361e
Yapici-lab <77295916+Nilayyapici@users.noreply.github.com>
2026-08-08 17:31:16 -0400

    Delete common_functions/findnearest.m

--- common_functions/findnearest.m
+++
@@ -1,5 +0,0 @@
-%% Helper function to find nearest value (replacement for findnearest)
-function [idx, val] = findNearest(target, array)
-    [~, idx] = min(abs(array - target));
-    val = array(idx);
-end
